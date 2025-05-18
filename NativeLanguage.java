b3965e955451962b82b18a610da0cca4884a2df2
kanakraghav1195 <Kanakraghav1195@gmail.com>
2025-05-18 20:05:22 +0530

    Native Language name
    Native Language name

---
+++ NativeLanguage.java
@@ -0,0 +1,15 @@
+class NativeLanguage {
+     public static void main(String[] args) {
+         char hindi = '\u0915';
+         char english = '\u0928';
+         char maths = '\u0930';
+         char science = '\u093E';
+         char bio = '\u0918';
+         char chem = '\u0935';
+         System.out.println("" + hindi + english + hindi + ' ' + maths + science + bio + chem);
+     }
+ }
+
+// output:
+// /Library/Java/JavaVirtualMachines/jdk-18.0.2.1.jdk/Contents/Home/bin/java -javaagent:/Applications/IntelliJ IDEA CE.app/Contents/lib/idea_rt.jar=65309 -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath /Users/kanakraghav/IdeaProjects/learning/out/production/learning I
+// कनक राघव
