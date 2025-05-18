decb6fa216968add725f7b1e0bf18445e8ba9dc3
kanakraghav1195 <Kanakraghav1195@gmail.com>
2025-05-18 20:46:43 +0530

    Swapping variable Without Third Variable
    Swapping variable Without Third Variable

---
+++ WithoutThirdVariable.java
@@ -0,0 +1,14 @@
+public class Test {
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 7;
+        i=(i+j); //i=17, j=7
+        j=(i-j); //j=10, i=17
+        i=(i-j); //i=7, j=10
+        System.out.println("i = " + i + "," + ' ' +"j = " + j);
+        
+    }
+}
+
+// output:
+// i = 7, j = 10
