7c23671b248a55a6ae00cc39100daf01d5e2939e
kanakraghav1195 <Kanakraghav1195@gmail.com>
2025-05-24 00:15:27 +0530

    Seconds Conversion

---
+++ src/SecondsToHour.java
@@ -0,0 +1,19 @@
+package src;
+
+import java.util.Scanner;
+
+public class SecondsToHour {
+    public static void main(String[] args) {
+        System.out.println("Enter your number");
+        Scanner a = new Scanner(System.in);
+        int b = a.nextInt();
+        int hours = b/(60*60);
+        int minutes = (b%3600)/60;
+        int seconds = (b%3600)%60;
+        System.out.print(hours + "hours ");
+        System.out.print( minutes + "minutes ");
+        System.out.print( seconds + "seconds");
+
+    }
+}
+
