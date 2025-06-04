9d9ccff4ede55bb986cfd520a18c1dee0dedabe7
kanakraghav1195 <Kanakraghav1195@gmail.com>
2025-06-04 21:54:47 +0530

    make some changes

--- src/SquareNumberFourthPower.java
+++ src/SquareNumberFourthPower.java
@@ -8,7 +8,7 @@ class Maths {
         System.out.println("Enter your number");
 
         int h= consoleRead.nextInt();
-        System.out.println("src.Square = " + h*h);
+        System.out.println("Square = " + h*h);
         System.out.println("Cube = " + h*h*h);
         System.out.println("Fourth power = " + h*h*h*h);
     }
@@ -17,28 +17,28 @@ class Maths {
 // output for 2:
 // Enter your number
 // 2
-// src.Square = 4
+// Square = 4
 // Cube = 8
 // Fourth power = 16
 
 // output for 10:
 // Enter your number
 // 10
-// src.Square = 100
+// Square = 100
 // Cube = 1000
 // Fourth power = 10000
 
 // output for 89:
 // Enter your number
 // 89
-// src.Square = 7921
+// Square = 7921
 // Cube = 704969
 // Fourth power = 62742241
 
 // output for 134:
 // Enter your number
 // 134
-// src.Square = 17956
+// Square = 17956
 // Cube = 2406104
 // Fourth power = 322417936
 
@@ -48,7 +48,7 @@ class Square {
         System.out.println("Enter your number");
 
         float h= consoleRead.nextFloat();
-        System.out.println("src.Square = " + h*h);
+        System.out.println("Square = " + h*h);
         System.out.println("Cube = " + h*h*h);
         System.out.println("Fourth power = " + h*h*h*h);
     }
@@ -61,7 +61,7 @@ class Square {
 // output for 34.5:
 // Enter your number
 // 34.5
-// src.Square = 1190.25
+// Square = 1190.25
 // Cube = 41063.625
 // Fourth power = 1416695.0
 
@@ -70,6 +70,6 @@ class Square {
 // output for -45:
 // Enter your number
 // -45
-// src.Square = 2025
+// Square = 2025
 // Cube = -91125
 // Fourth power = 4100625
