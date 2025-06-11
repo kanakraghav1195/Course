619e26fe78bb2122bdccaf5da3dd535e1f2545ca
kanakraghav1195 <Kanakraghav1195@gmail.com>
2025-06-11 21:43:26 +0530

    Find factor for Number

---
+++ src/FactorForNumber.java
@@ -0,0 +1,13 @@
+package src;
+
+public class FactorForNumber {
+    public static void main(String[] args) {
+        int i = 24;
+        for(int j =1; j<25; j++){
+            if(i%j==0){
+                System.out.print( j + " ");
+            }
+
+        }
+    }
+}
