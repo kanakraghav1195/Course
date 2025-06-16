064523791b5e7bb3458afe8b52e29bc50be1d729
kanakraghav1195 <Kanakraghav1195@gmail.com>
2025-06-16 23:06:13 +0530

    To make 17 with provided operators and numbers only

---
+++ src/CodeToMake17.java
@@ -0,0 +1,13 @@
+package src;
+
+public class CodeToMake17 {
+    public static void main(String[] args) {
+        int a=1;
+        int b =2;
+        int c =6;
+        int d = 7;
+
+        int result = (c-a)*b+d;
+        System.out.println("result = " + result);
+    }
+}
