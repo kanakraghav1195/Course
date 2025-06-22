2bbbdb51ed7787c93f52e0ca498b1c4f344b6bc3
kanakraghav1195 <Kanakraghav1195@gmail.com>
2025-06-22 18:54:08 +0530

    Money Return coin change program

---
+++ src/MoneyReturnCoinChange.java
@@ -0,0 +1,27 @@
+package src;
+
+import java.util.Scanner;
+
+public class MoneyReturnCoinChange {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter Bought Items for");
+        int boughtItemsFor = scan.nextInt();
+        System.out.println("Enter Given Rupee");
+        int givenRupee = scan.nextInt();
+
+        int totalAmountReturn = givenRupee - boughtItemsFor;
+        System.out.println("Total return = " + totalAmountReturn);
+
+
+        while(totalAmountReturn>= 20){
+            System.out.println("Note: " + 20);
+            totalAmountReturn = totalAmountReturn-20;
+        }
+        while(totalAmountReturn>= 2){
+            System.out.println("Coin: " + 2);
+            totalAmountReturn = totalAmountReturn-2;
+        }
+
+    }
+}
