1ae744f20e577701f4956bc2cb5921fda5267785
kanakraghav1195 <Kanakraghav1195@gmail.com>
2025-06-22 19:01:29 +0530

    Make some changes

--- src/MoneyReturnCoinChange.java
+++ src/MoneyReturnCoinChange.java
@@ -18,9 +18,29 @@ public class MoneyReturnCoinChange {
             System.out.println("Note: " + 20);
             totalAmountReturn = totalAmountReturn-20;
         }
+        while(totalAmountReturn>= 10){
+            System.out.println("Note: " + 10);
+            totalAmountReturn = totalAmountReturn-10;
+        }
+        while(totalAmountReturn>= 50){
+            System.out.println("Note: " + 50);
+            totalAmountReturn = totalAmountReturn-50;
+        }
+        while(totalAmountReturn>= 100){
+            System.out.println("Note: " + 100);
+            totalAmountReturn = totalAmountReturn-100;
+        }
         while(totalAmountReturn>= 2){
             System.out.println("Coin: " + 2);
             totalAmountReturn = totalAmountReturn-2;
+        }
+        while(totalAmountReturn>=1){
+            System.out.println("Coin: " + 1);
+            totalAmountReturn = totalAmountReturn-1;
+        }
+        while(totalAmountReturn>=5){
+            System.out.println("Coin: " + 5);
+            totalAmountReturn = totalAmountReturn-5;
         }
 
     }
