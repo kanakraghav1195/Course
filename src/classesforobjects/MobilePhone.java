229a1da6c788bf6f59b0d4c1b170be32629b2a16
kanakraghav1195 <Kanakraghav1195@gmail.com>
2025-07-10 20:44:44 +0530

    added method

--- src/classesforobjects/MobilePhone.java
+++ src/classesforobjects/MobilePhone.java
@@ -1,5 +1,7 @@
 package src.classesforobjects;
 
+import java.sql.SQLOutput;
+
 public class MobilePhone {
     String modelName;
     String color;
@@ -9,5 +11,14 @@ public class MobilePhone {
     double price;
     double screenSize;
 
+    public void mobilePhoneInformation(){
+        System.out.println("Mobile model: " + modelName);
+        System.out.println("Available color: " + color);
+        System.out.println("Battery Capacity: " + batteryCapacity + "mAh");
+        System.out.println("Storage of Phone: " + storage + "GB");
+        System.out.println("RAM: " + ram + "GB");
+        System.out.println("Price: Rs. " + price);
+        System.out.println("Size of Screen: " + screenSize);
+    }
 
 }
