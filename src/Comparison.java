d89afe7d1dc7534ae2c24cc19eff414b5442f1c8
kanakraghav1195 <Kanakraghav1195@gmail.com>
2025-08-05 12:39:08 +0530

    Comparison for equals operator and equals method

---
+++ src/Comparison.java
@@ -0,0 +1,11 @@
+package src;
+
+public class Comparison {
+    public static void main(String[] args) {
+        String first = "My name is Kanak";
+        String second = "My Name Is Kanak";
+
+        System.out.println(first == second);
+        System.out.println("first.equals(second): " + first.equals(second));
+    }
+}
