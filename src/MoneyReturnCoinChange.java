9ecff938555d6db5e950473e35945d4f27b8a394
kanakraghav1195 <Kanakraghav1195@gmail.com>
2025-08-11 14:41:09 +0530

    Change the approach

--- src/MoneyReturnCoinChange.java
+++ src/MoneyReturnCoinChange.java
@@ -7,41 +7,25 @@ public class MoneyReturnCoinChange {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter Bought Items for");
         int boughtItemsFor = scan.nextInt();
+
         System.out.println("Enter Given Rupee");
         int givenRupee = scan.nextInt();
 
         int totalAmountReturn = givenRupee - boughtItemsFor;
         System.out.println("Total return = " + totalAmountReturn);
 
+        int[] rupee = {100, 50, 20, 10, 5, 2, 1};
 
-        while(totalAmountReturn>= 20){
-            System.out.println("Note: " + 20);
-            totalAmountReturn = totalAmountReturn-20;
+        for(int i =0; i<rupee.length; i++){
+        while(totalAmountReturn>=rupee[i]){
+            if(rupee[i]>=10){
+                System.out.println("Note: " + rupee[i]);
+            }else{
+                System.out.println("Coins: " + rupee[i]);
+            }
+            totalAmountReturn= totalAmountReturn - rupee[i];
         }
-        while(totalAmountReturn>= 10){
-            System.out.println("Note: " + 10);
-            totalAmountReturn = totalAmountReturn-10;
         }
-        while(totalAmountReturn>= 50){
-            System.out.println("Note: " + 50);
-            totalAmountReturn = totalAmountReturn-50;
-        }
-        while(totalAmountReturn>= 100){
-            System.out.println("Note: " + 100);
-            totalAmountReturn = totalAmountReturn-100;
-        }
-        while(totalAmountReturn>= 2){
-            System.out.println("Coin: " + 2);
-            totalAmountReturn = totalAmountReturn-2;
-        }
-        while(totalAmountReturn>=1){
-            System.out.println("Coin: " + 1);
-            totalAmountReturn = totalAmountReturn-1;
-        }
-        while(totalAmountReturn>=5){
-            System.out.println("Coin: " + 5);
-            totalAmountReturn = totalAmountReturn-5;
-        }
-
     }
+
 }
