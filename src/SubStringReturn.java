75d539b68c9c7ce4585522f897838c9d3aed8933
kanakraghav1195 <Kanakraghav1195@gmail.com>
2025-08-25 22:50:42 +0530

    Adding new example for return a portion in string

---
+++ src/SubStringReturn.java
@@ -0,0 +1,26 @@
+package src;
+
+public class SubStringReturn {
+
+    public static String returnPart(String str, int index) {
+                if (index < 0 || index >= str.length()) {
+                    index = 0;
+                }
+                if (index == str.length() - 1) {
+                    return str.charAt(index) + "" + str.charAt(0);
+                }
+
+                // return substring of length 2
+                return str.substring(index, index + 2);
+            }
+            public static void main(String[] args) {
+                // Testcases
+                System.out.println(returnPart("happen", 3));
+                System.out.println(returnPart("hello", 0));
+                System.out.println(returnPart("hello", 99));
+                System.out.println(returnPart("yay", 1));
+                System.out.println(returnPart("java", 0));
+                System.out.println(returnPart("java", 2));
+                System.out.println(returnPart("java", 3));
+            }
+        }
